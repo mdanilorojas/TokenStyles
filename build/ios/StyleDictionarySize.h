b07aea27cabcf5c5052dbece7da852bc36ddeb57
mdanilorojas <111707024+mdanilorojas@users.noreply.github.com>
2022-08-25 16:30:27 -0500

    first commit

---
+++ build/ios/StyleDictionarySize.h
@@ -0,0 +1,15 @@
+
+// StyleDictionarySize.h
+//
+
+// Do not edit directly
+// Generated on Thu, 25 Aug 2022 21:25:12 GMT
+
+
+#import <Foundation/Foundation.h>
+
+
+extern float const SizeFontSmall;
+extern float const SizeFontMedium;
+extern float const SizeFontLarge;
+extern float const SizeFontBase;
